e3829b654120fe380ce3a4d670e5112de0b941e2
AlkarosLux <metal.djinn@gmail.com>
2026-07-14 00:31:11 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='fiche-site-eib-v7';
-const ASSETS=['./','./index.html','./styles.css','./fix-v5.css','./app.js?v=7','./docx-lib.js?v=4','./manifest.webmanifest','./icon.svg','./icon-180.png','./icon-192.png','./icon-512.png'];
+const CACHE='fiche-site-eib-v8';
+const ASSETS=['./','./index.html','./styles.css','./fix-v5.css','./app.js?v=7','./docx-lib.js?v=4','./manifest.webmanifest?v=8','./icon.svg','./icon-180.png?v=8','./icon-192.png?v=8','./icon-512.png?v=8'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('./index.html'))))});
